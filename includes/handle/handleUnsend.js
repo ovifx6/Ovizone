c3cd11b74305098cfc9740179cd42ec7f9cfc6cb
rxabdullah0007 <rxabdullah0007@gmail.com>
2025-12-18 00:19:20 +0600

    Delete includes/handle directory

--- includes/handle/handleUnsend.js
+++
@@ -1,9 +0,0 @@
-module.exports = function({ api, Users }) {
-  return function({ event, Users, args }) {
-        const { senderID, reaction, messageID, threadID, userID } = event;
-        if (senderID == api.getCurrentUserID()) {
-            if (reaction == "😠") return api.unsendMessage(messageID);
-        }
-    }
-
-}
